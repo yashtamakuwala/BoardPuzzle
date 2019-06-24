679c99886c44458f9a54b120b7848515531fbf98
yashtamakuwala <neel64tamakuwala@gmail.com>
2019-06-24 15:25:22 +1000

    Create puzzle.c

---
+++ puzzle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h> 
+#include "boardADT.h"
+#include <math.h>
+
+int main()
+{
+    Board Board1;
+    int size1=createBoard(Board1,2);
+    char c;
+    
+    // int size = sqrt(i);
+    // display_board(number_stream, size);
+    return EXIT_SUCCESS;
+}
+
